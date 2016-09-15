ba3ed7d914bf8ae4488d21e97370766b23214d5d
lbn99 <ninjatuna2014@gmail.com>
2016-09-15 11:08:04 -0500

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+//	private Vector3 radius;
+
+	void Start ()
+	{
+		offset /*= radius*/ = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+		transform.Rotate (new Vector3(0.0f, Input.GetAxis("Horizontal") / 5, 0.0f));
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//		Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
+
+//		rb.AddRelativeForce (movement * speed);
+//		gameObject.transform.Rotate (new Vector3 (0.0f, moveHorizontal, 0.0f) * Time.deltaTime * 20.0f);
+		rb.AddForce(movement * speed);
+	}
+}
